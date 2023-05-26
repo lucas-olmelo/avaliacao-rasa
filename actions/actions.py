aff10583a1b2344a689e718789436af866828416
Lucas Melo <112345359+lucas-olmelo@users.noreply.github.com>
2023-05-25 21:14:35 -0300

    Novo actions

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,43 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import datetime
+
+class Action_Mostra_Boleto():
+
+    def name(self) -> Text:
+        return "action_mostra_boleto"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dia = datetime.date.today().day
+
+        if (dia <= 15):
+            conta_em_dia = True
+        else:
+            conta_em_dia = False
+
+        if (conta_em_dia == False):
+            dias_atraso = dia - 16
+            valor_conta = 149.90
+
+            juros_dia = valor_conta * 0.02
+            juros_totais = juros_dia * dias_atraso
+            valor_com_juros = valor_conta + juros_totais
+
+            txt_confirmacao = "O valor da sua conta é R$"+ valor_com_juros
+
+        else:
+            txt_confirmacao = "O valor da sua conta é R$149.90"
+
+        dispatcher.utter_message(text=txt_confirmacao)
+        return []
+
+
+
